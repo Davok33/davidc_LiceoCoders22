870e21a784df359c1cd422e65a7fc7d214cbd20e
David C <101876023+Davok33@users.noreply.github.com>
2022-05-30 15:01:27 +0200

    Ejercicios

---
+++ Apuntes/Javascript Mayo 30/clase1.js
@@ -0,0 +1,77 @@
+/*  las comillas interpretan como texto 
+10 + "10" = 1000 lo que haces es en ves de hacer la operación los une y da 1000
+10 y 10 1000
+
+el parseInt("") convierte el texto en numero y esto si hace la operacion en este ejem
+10 + parseInt("10") = 20
+
+isNaN es un no numero
+
+n = "10"
+
+if(isNaN("10"))
+
+array 
+var = ["manzana", "platano"]
+
+lenght numeros
+frutas[0] --- "manzana" se empieza desde 0
+var nuevaLongi = frutas.push("naranja") añade naranja al array
+nueva longitud -- 3
+
+objetos es similar al array pero no con posiciones sino con atritubos
+
+Objetos
+
+var coche = new Object();
+coche.marca = "Ford";
+coche.modelo = "Mustang";
+coche.year = 1970;
+
+var c1 = new Object()
+c1.fecha = "30/10/22"
+c1.lugar = "Logroño"
+
+var c2 = new Object()
+c2.fecha = "30/10/22"
+c2.lugar = "Logroño"
+
+si pongo c1 o c2 me muestra e array de lo que tienen
+
+(())
+
+
+conciertos = [c1, c2]
+
+[{...}], [{...}]
+
+si escribo conciertos me muestra
+te suma los dos c1 y c2
+
+coche.marca = ford       si pones un espacio no lo lee
+coche["modelo"] = fiesta      puedes poner espacios y eso
+
+var funciona en todo el codigo
+let funciona en el bloque que estoy en estos momentos
+
+let si esta dentro de la funcion solo funciona alli, sy esta afuera
+la funcion no la reconoce
+
+con var es global las funciones las pueden declarar
+
+
+const apellido = "perez"
+apellido = "lopez"    ----error
+
+los valores de las constantes no se pueden cambiar
+
+
+funciones anidadas
+function squareAndMultiply(num1, num2){
+    function square(x)
+}
+
+
+
+
+*/
