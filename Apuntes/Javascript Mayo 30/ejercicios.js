61dfb7d7cc1155997426481fdf70a1f2e643d5f8
David C <101876023+Davok33@users.noreply.github.com>
2022-05-31 11:24:00 +0200

    1

--- Apuntes/Javascript Mayo 30/ejercicios.js
+++ Apuntes/Javascript Mayo 30/ejercicios.js
@@ -12,10 +12,10 @@ function perimetro(radio){
  console.log(area(20)) /* aca que me imprima en la consola el area dependiendo del valor que le de */
 
  /* otra forma de hacer introduciendo el valor */
- const pi = Math.PI
-	var a,radio;
+ const pi = Math.PI //con a constante no se puede modificar el valor de variable
+	var a,radio; //llaman nombre las variables a y radio
 	radio = parseFloat(prompt("Ingresar el radio")); /* parseFloat devuelve el string como flotante y prompt ontiene un cuadro de texto de una línea, un botón Cancel (Cancelar) un botón OK (Aceptar), y devuelve el texto*/
-	a = pi*radio*radio;
+	a = pi*radio*radio; //a es igual a pi * radio que es lo mismo pi ** radio
 	alert("El área es " + a);
 
     function calcArea()
@@ -35,12 +35,17 @@ function perimetro(radio){
         <input type="button" value="calcular" onclick=Area()>     ------   se crea un input que sea del tipo boton llamando la funcion en el evento onclick para que nos muestre en pantalla el resultado
      </form> */
 
-     function calcHipotenusa()
+     function calcHipotenusa() //ponemos la funcion y el nombre de ella
      {
-         let lado_a, lado_b, hipotenusa;
-         lado_a = parseFloat ("lado a");
+         let lado_a, lado_b, hipotenusa;   //declaramos las 3 variables
+         lado_a = parseFloat ("lado a"); //el parseFloat nos devuelve el numero de un string como flotante,
          lado_b = parseFloat ("lado b");
-         hipotenusa=Math.sqrt(lado_a*lado_a+lado_b*lado_b);
+          return hipotenusa = Math.hypot(cuadrado(lado_a) + cuadrado(lado_b)); // esta es la operacion para sacar la hipotenusa Math.hypot es la operacion
         alert("la hipotenusa es" + hipotenusa);
      }
+	//damos los valores con html para sacar los numeros
+ /* <form name="form1">
+        <input type="button" value="calcular" onclick=Area()>     ------   se crea un input que sea del tipo boton llamando la funcion en el evento onclick para que nos muestre en pantalla el resultado
+     </form> */
+
      
